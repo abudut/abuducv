3d7a9c4053ebec74a6148153bf58e4f2c420c2f2
Abudu <abudutouray1999.at@gmail.com>
2022-10-05 20:32:22 +0100

    first commit

--- components/Project.js
+++ components/Project.js
@@ -12,6 +12,44 @@ function Project() {
           </h1>
 
           <div className="grid grid-cols-1 gap-3 md:grid-cols-3 ">
+          <div className="p-2 hover:p-1  border-2 hover:border-4 hover:brightness-105 border-green-500  opacity-100 lg:opacity-70  hover:opacity-100 transition duration-300 ease-in sm:hover:scale-105 hover:z-50">
+              <img
+                className="object-center h-auto w-auto mb-1"
+                src="images\hulu.PNG"
+                alt="huluClone"
+              />
+              <div className="flex justify-between">
+                <a
+                  href="https://hulu-clon-e24z.vercel.app/"
+                  title="HuluClone"
+                >
+                  <EyeIcon className="h-6 w-6" />
+                </a>{" "}
+                <a href="https://github.com/abudut/HuluClon">
+                  <CodeIcon className="h-6 w-6" />
+                </a>
+              </div>
+            </div>
+            
+          <div className="p-2 hover:p-1 border-2  hover:border-4 hover:brightness-105 border-green-500 opacity-100 lg:opacity-70  hover:opacity-100 transition duration-300 ease-in sm:hover:scale-105 hover:z-50">
+              <img
+                className="object-center h-auto w-auto mb-1"
+                src="images\amazon.PNG"
+                alt="AmazonClone"
+              />
+              <div className="flex justify-between">
+                <a
+                  href="https://amazon-a-abudut.vercel.app/"
+                  title="AmazonClone"
+                >
+                  <EyeIcon className="h-6 w-6" />
+                </a>{" "}
+                <a href="https://github.com/abudut/AmazonA">
+                  <CodeIcon className="h-6 w-6" />
+                </a>
+              </div>
+            </div>
+
             <div className="p-2 hover:p-1 border-2 hover:border-4 hover:brightness-105 border-green-500  opacity-100 lg:opacity-70  hover:opacity-100 transition duration-300 ease-in  sm:hover:scale-105 hover:z-50">
               <img
                 className="object-center  h-auto w-auto mb-1"
@@ -30,42 +68,7 @@ function Project() {
                 </a>
               </div>
             </div>
-            <div className="p-2 hover:p-1  border-2 hover:border-4 hover:brightness-105 border-green-500  opacity-100 lg:opacity-70  hover:opacity-100 transition duration-300 ease-in sm:hover:scale-105 hover:z-50">
-              <img
-                className="object-center h-auto w-auto mb-1"
-                src="images\hulu.PNG"
-                alt="huluClone"
-              />
-              <div className="flex justify-between">
-                <a
-                  href="https://hulu-clon-e24z.vercel.app/"
-                  title="HuluClone"
-                >
-                  <EyeIcon className="h-6 w-6" />
-                </a>{" "}
-                <a href="https://github.com/abudut/HuluClon">
-                  <CodeIcon className="h-6 w-6" />
-                </a>
-              </div>
-            </div>
-            <div className="p-2 hover:p-1 border-2  hover:border-4 hover:brightness-105 border-green-500 opacity-100 lg:opacity-70  hover:opacity-100 transition duration-300 ease-in sm:hover:scale-105 hover:z-50">
-              <img
-                className="object-center h-auto w-auto mb-1"
-                src="images\amazon.PNG"
-                alt="AmazonClone"
-              />
-              <div className="flex justify-between">
-                <a
-                  href="https://amazon-a-abudut.vercel.app/"
-                  title="AmazonClone"
-                >
-                  <EyeIcon className="h-6 w-6" />
-                </a>{" "}
-                <a href="https://github.com/abudut/AmazonA">
-                  <CodeIcon className="h-6 w-6" />
-                </a>
-              </div>
-            </div>
+        
           </div>
         </div>
       </div>
